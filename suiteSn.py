59585e3151e6933077479d6abb63f090357bdda3
rarob54 <rarob54@ulaval.ca>
2026-01-31 16:21:39 -0500

    Avancement

--- suiteSn.py
+++ suiteSn.py
@@ -2,5 +2,9 @@ import numpy as np
 from math import e
 
 def suiteSn(n):
-    S = np.array([e-1])
+    S = np.array([np.e-1])
+
+    for i in range(1, n+1):
+        Si = np.e - i * S[i-1]
+        S = np.append(S, Si)
     return S
