75ab39dcbc9dfa09d9cd070dadfc60dd4bce405d
Sara Rangel <saran15@ulaval.ca>
2026-02-08 14:36:56 -0500

    pep8

--- equipe111.py
+++ equipe111.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from suiteSn import suiteSn
 
-#1
+# 1
 a = np.full((6, 1), 1)
 b = np.arange(1, 7)
 c = a.reshape(1, 6)
@@ -10,70 +10,73 @@ d = 111 * c
 I = np.identity(6)
 J = np.full((6, 6), 1)
 K = np.diag(b)
-L = (55*I) - J + (2*(a@c))
-M = K; M[:, 0] = a.reshape(6,)
+L = (55 * I) - J + (2 * (a @ c))
+M = K; M[:, 0] = a.reshape(6, )
 dd = np.linalg.det(M)
 x = np.linalg.solve(M, a)
 N = np.linalg.solve(M, M.T)
 
-print('a = ', a, sep = '\n', end = '\n\n')
-print('b = ', b, sep = '\n', end = '\n\n')
-print('c = ', c, sep = '\n', end = '\n\n')
-print('d = ', d, sep = '\n', end = '\n\n')
-print('I = ', I, sep = '\n', end = '\n\n')
-print('J = ', J, sep = '\n', end = '\n\n')
-print('K = ', K, sep = '\n', end = '\n\n')
-print('L = ', L, sep = '\n', end = '\n\n')
-print('M = ', M, sep = '\n', end = '\n\n')
-print('dd = ', dd, sep = '\n', end = '\n\n')
-print('x = ', x, sep = '\n', end = '\n\n')
-print('N = ', N, sep = '\n', end = '\n\n')
+print('a = ', a, sep='\n', end='\n\n')
+print('b = ', b, sep='\n', end='\n\n')
+print('c = ', c, sep='\n', end='\n\n')
+print('d = ', d, sep='\n', end='\n\n')
+print('I = ', I, sep='\n', end='\n\n')
+print('J = ', J, sep='\n', end='\n\n')
+print('K = ', K, sep='\n', end='\n\n')
+print('L = ', L, sep='\n', end='\n\n')
+print('M = ', M, sep='\n', end='\n\n')
+print('dd = ', dd, sep='\n', end='\n\n')
+print('x = ', x, sep='\n', end='\n\n')
+print('N = ', N, sep='\n', end='\n\n')
 
 plt.matshow(N, cmap='RdYlBu')
 plt.colorbar()
-plt.title("Figure 1 \n Matrice N", fontsize = 20, color = 'magenta')
+plt.title("Figure 1 \n Matrice N", fontsize=20, color='magenta')
 plt.figure(1)
 plt.show()
 
 x = np.linspace(0, 1, 101)
 
+
 def fonction(x):
-    f = ((-x**2)/2) + (np.exp(x)) + (np.sin(x))
+    f = ((-x**2) / 2) + (np.exp(x)) + (np.sin(x))
     return f
+
+
 plt.figure(2)
-plt.plot(x,fonction(x))
-plt.xlabel("x", fontsize = 20, color = 'black')
-plt.ylabel("f(x)", fontsize = 20, color = 'black')
+plt.plot(x, fonction(x))
+plt.xlabel("x", fontsize=20, color='black')
+plt.ylabel("f(x)", fontsize=20, color='black')
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
-plt.title("Figure 2 \n Fonction f(x) sur l'intervalle [0,1]", fontsize = 20, color = 'brown')
+plt.title("Figure 2 \n Fonction f(x) sur l'intervalle [0,1]", fontsize=20, color='brown')
 plt.show()
 
-#2
+# 2
 n = 19
 x = np.arange(0, n + 1)
 plt.figure(3)
 plt.plot(x, suiteSn(n))
-plt.xlabel("n", fontsize = 20, color = 'black')
-plt.ylabel("Sn", fontsize = 20, color = 'black')
+plt.xlabel("n", fontsize=20, color='black')
+plt.ylabel("Sn", fontsize=20, color='black')
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
 plt.xticks(np.arange(0, 20, 2))
 plt.title("Figure 3 \n Sn en fonction de n")
 plt.show()
 
-#3
+# 3
 deriveefonction = 2.0
 h = 10.0**(-np.arange(1, 13))
-D = (fonction(h) - fonction(0))/h
-#print(D)
+D = (fonction(h) - fonction(0)) / h
+# print(D)
 erreur = np.abs(deriveefonction - D)
-#print(erreur)
+# print(erreur)
 plt.figure(4)
 plt.loglog(h, erreur, marker="o")
-plt.xlabel("h", fontsize = 20, color = "black")
-plt.ylabel("Erreur", fontsize = 20, color = "black")
+plt.xlabel("h", fontsize=20, color="black")
+plt.ylabel("Erreur", fontsize=20, color="black")
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
-plt.title("Erreur en fonction de h (échelle loglog)", fontsize = 20, color = "blue")
+plt.title("Erreur en fonction de h (échelle loglog)", fontsize=20, color="blue")
 plt.show()
--- suiteSn.py
+++ suiteSn.py
@@ -1,12 +1,14 @@
 import numpy as np
 
-def suiteSn(n):
-    S = np.array([np.e-1])
 
-    for i in range(1, n+1):
-        Si = np.e - i * S[i-1]
+def suiteSn(n):
+    S = np.array([np.e - 1])
+
+    for i in range(1, n + 1):
+        Si = np.e - i * S[i - 1]
         S = np.append(S, Si)
     return S
+
 
 test18 = suiteSn(18)
 print(test18)
